32c5ccd5e2afa31ea0781aea01fbafd72d8ed35c
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2023-04-05 21:39:01 +0200

    Create Book.cs

---
+++ Data_Structure_Assigment_Y2/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This is a class that represents a book
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    public string Genre { get; set; }
+    public string Publisher { get; set; }
+}
