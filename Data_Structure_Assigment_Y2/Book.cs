9e2d250a25cff0c121c4f8d66f1d019830fb6bb1
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2023-04-05 21:57:18 +0200

    Adding book class used for converting JSON elements into objects

--- Data_Structure_Assigment_Y2/Book.cs
+++ Data_Structure_Assigment_Y2/Book.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Data_Structure_Assigment_Y2
+// This is a class that represents a book
+public class Book
 {
-    internal class Book
-    {
-    }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    public string Genre { get; set; }
+    public string Publisher { get; set; }
 }
