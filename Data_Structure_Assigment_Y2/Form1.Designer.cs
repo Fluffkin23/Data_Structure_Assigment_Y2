3f11f8d5b1a2b919ed10e4e104ffc90fc3505d23
Husarescu Rares <husarescu.rares.01@gmail.com>
2023-04-05 22:23:59 +0200

    GUI for the application

--- Data_Structure_Assigment_Y2/Form1.Designer.cs
+++ Data_Structure_Assigment_Y2/Form1.Designer.cs
@@ -28,13 +28,302 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.HalfRichBoxText = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.DictionaryRadio = new System.Windows.Forms.RadioButton();
+            this.HashSetRadio = new System.Windows.Forms.RadioButton();
+            this.LinkedListRadio = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.JsonRichBox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.InsertionRadio = new System.Windows.Forms.RadioButton();
+            this.BubbleRadio = new System.Windows.Forms.RadioButton();
+            this.LinearButton = new System.Windows.Forms.RadioButton();
+            this.BinaryButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(293, 490);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.TabIndex = 49;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // HalfRichBoxText
+            // 
+            this.HalfRichBoxText.Location = new System.Drawing.Point(1263, 121);
+            this.HalfRichBoxText.Name = "HalfRichBoxText";
+            this.HalfRichBoxText.Size = new System.Drawing.Size(348, 389);
+            this.HalfRichBoxText.TabIndex = 48;
+            this.HalfRichBoxText.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 16);
+            this.label4.TabIndex = 47;
+            this.label4.Text = "Item to Search:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 46;
+            this.label3.Text = "Sort by :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DictionaryRadio);
+            this.groupBox1.Controls.Add(this.HashSetRadio);
+            this.groupBox1.Controls.Add(this.LinkedListRadio);
+            this.groupBox1.Location = new System.Drawing.Point(12, 129);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(210, 124);
+            this.groupBox1.TabIndex = 44;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Data Structure";
+            // 
+            // DictionaryRadio
+            // 
+            this.DictionaryRadio.AutoSize = true;
+            this.DictionaryRadio.Location = new System.Drawing.Point(16, 99);
+            this.DictionaryRadio.Name = "DictionaryRadio";
+            this.DictionaryRadio.Size = new System.Drawing.Size(88, 20);
+            this.DictionaryRadio.TabIndex = 18;
+            this.DictionaryRadio.TabStop = true;
+            this.DictionaryRadio.Text = "Dictionary";
+            this.DictionaryRadio.UseVisualStyleBackColor = true;
+            // 
+            // HashSetRadio
+            // 
+            this.HashSetRadio.AutoSize = true;
+            this.HashSetRadio.Location = new System.Drawing.Point(16, 61);
+            this.HashSetRadio.Name = "HashSetRadio";
+            this.HashSetRadio.Size = new System.Drawing.Size(83, 20);
+            this.HashSetRadio.TabIndex = 17;
+            this.HashSetRadio.TabStop = true;
+            this.HashSetRadio.Text = "Hash Set";
+            this.HashSetRadio.UseVisualStyleBackColor = true;
+            // 
+            // LinkedListRadio
+            // 
+            this.LinkedListRadio.AutoSize = true;
+            this.LinkedListRadio.Location = new System.Drawing.Point(16, 24);
+            this.LinkedListRadio.Name = "LinkedListRadio";
+            this.LinkedListRadio.Size = new System.Drawing.Size(91, 20);
+            this.LinkedListRadio.TabIndex = 16;
+            this.LinkedListRadio.TabStop = true;
+            this.LinkedListRadio.Text = "Linked List";
+            this.LinkedListRadio.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 533);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 43;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // JsonRichBox
+            // 
+            this.JsonRichBox.Location = new System.Drawing.Point(877, 121);
+            this.JsonRichBox.Name = "JsonRichBox";
+            this.JsonRichBox.Size = new System.Drawing.Size(348, 389);
+            this.JsonRichBox.TabIndex = 42;
+            this.JsonRichBox.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 41;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(276, 165);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 24);
+            this.comboBox1.TabIndex = 40;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(267, 305);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 22);
+            this.textBox1.TabIndex = 39;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(503, 121);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(340, 389);
+            this.richTextBox1.TabIndex = 37;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(734, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(338, 32);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "Data Structure Algorithms";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1653, 28);
+            this.menuStrip1.TabIndex = 38;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
+            this.importToolStripMenuItem.Text = "Import";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.InsertionRadio);
+            this.groupBox2.Controls.Add(this.BubbleRadio);
+            this.groupBox2.Controls.Add(this.LinearButton);
+            this.groupBox2.Controls.Add(this.BinaryButton);
+            this.groupBox2.Location = new System.Drawing.Point(12, 280);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(214, 218);
+            this.groupBox2.TabIndex = 45;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select Algorithm";
+            // 
+            // InsertionRadio
+            // 
+            this.InsertionRadio.AutoSize = true;
+            this.InsertionRadio.Location = new System.Drawing.Point(20, 25);
+            this.InsertionRadio.Name = "InsertionRadio";
+            this.InsertionRadio.Size = new System.Drawing.Size(105, 20);
+            this.InsertionRadio.TabIndex = 19;
+            this.InsertionRadio.TabStop = true;
+            this.InsertionRadio.Text = "Insertion Sort";
+            this.InsertionRadio.UseVisualStyleBackColor = true;
+            // 
+            // BubbleRadio
+            // 
+            this.BubbleRadio.AutoSize = true;
+            this.BubbleRadio.Location = new System.Drawing.Point(20, 63);
+            this.BubbleRadio.Name = "BubbleRadio";
+            this.BubbleRadio.Size = new System.Drawing.Size(98, 20);
+            this.BubbleRadio.TabIndex = 20;
+            this.BubbleRadio.TabStop = true;
+            this.BubbleRadio.Text = "Bubble Sort";
+            this.BubbleRadio.UseVisualStyleBackColor = true;
+            // 
+            // LinearButton
+            // 
+            this.LinearButton.AutoSize = true;
+            this.LinearButton.Location = new System.Drawing.Point(20, 89);
+            this.LinearButton.Name = "LinearButton";
+            this.LinearButton.Size = new System.Drawing.Size(111, 20);
+            this.LinearButton.TabIndex = 21;
+            this.LinearButton.TabStop = true;
+            this.LinearButton.Text = "Linear Search";
+            this.LinearButton.UseVisualStyleBackColor = true;
+            // 
+            // BinaryButton
+            // 
+            this.BinaryButton.AutoSize = true;
+            this.BinaryButton.Location = new System.Drawing.Point(20, 135);
+            this.BinaryButton.Name = "BinaryButton";
+            this.BinaryButton.Size = new System.Drawing.Size(112, 20);
+            this.BinaryButton.TabIndex = 22;
+            this.BinaryButton.TabStop = true;
+            this.BinaryButton.Text = "Binary Search";
+            this.BinaryButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1653, 580);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.HalfRichBoxText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.JsonRichBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox HalfRichBoxText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton DictionaryRadio;
+        private System.Windows.Forms.RadioButton HashSetRadio;
+        private System.Windows.Forms.RadioButton LinkedListRadio;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox JsonRichBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton InsertionRadio;
+        private System.Windows.Forms.RadioButton BubbleRadio;
+        private System.Windows.Forms.RadioButton LinearButton;
+        private System.Windows.Forms.RadioButton BinaryButton;
     }
 }
 
