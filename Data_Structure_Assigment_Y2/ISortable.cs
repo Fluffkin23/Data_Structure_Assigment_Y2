8d6f39d08ddd59abec2dcae63589d7aa0afb037d
Husarescu Rares <husarescu.rares.01@gmail.com>
2023-04-05 22:02:29 +0200

    Interface for sorting algorithm

---
+++ Data_Structure_Assigment_Y2/ISortable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This is an interface that represents a sortable object
+public interface ISortable<T>
+{
+    void BubbleSort(SortBy sortBy);
+    void InsertionSort(SortBy sortBy);
+}
