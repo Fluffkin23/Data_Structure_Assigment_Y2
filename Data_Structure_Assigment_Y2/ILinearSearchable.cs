7177c199656c71c7c7c05b9eba34ff43e959048c
Husarescu Rares <husarescu.rares.01@gmail.com>
2023-04-05 21:59:35 +0200

    Interface used for the searching algorithms

---
+++ Data_Structure_Assigment_Y2/ILinearSearchable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure_Assigment_Y2
+{
+    internal class ILinearSearchable
+    {
+    }
+}
